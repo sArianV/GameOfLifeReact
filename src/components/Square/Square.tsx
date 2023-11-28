e4935e89f7f4a067b11be6dff26d3f6aee82aa5a
sArianV <yoarian94@gmail.com>
2023-11-28 18:05:46 -0300

    changed template for eslint

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,33 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-const Square = ({ status }: {
-    status: boolean
-}) => {
-    return (
-        <div
-            style={!status ? {
-                width: "1rem",
-                height: "1rem",
-                backgroundColor: 'white',
-                borderRight: "1px",
-                borderRightStyle: "solid",
-                borderRightColor: 'lightgray',
-                borderBottom: "1px",
-                borderBottomStyle: "solid",
-                borderBottomColor: 'lightgray'
-            } : {
-                width: "1rem",
-                height: "1rem",
-                backgroundColor: 'darkviolet',
-                borderRight: "1px",
-                borderRightStyle: "solid",
-                borderRightColor: 'lightgray',
-                borderBottom: "1px",
-                borderBottomStyle: "solid",
-                borderBottomColor: 'lightgray'
-            }}
-        />
-    )
-}
+const Square = ({ status }: { status: boolean }) => {
+  return (
+    <div
+      style={
+        !status
+          ? {
+              width: '.8rem',
+              height: '.8rem',
+              backgroundColor: 'white',
+              borderRight: '1px',
+              borderRightStyle: 'solid',
+              borderRightColor: 'lightgray',
+              borderBottom: '1px',
+              borderBottomStyle: 'solid',
+              borderBottomColor: 'lightgray',
+            }
+          : {
+              width: '.8rem',
+              height: '.8rem',
+              backgroundColor: 'darkviolet',
+              borderRight: '1px',
+              borderRightStyle: 'solid',
+              borderRightColor: 'lightgray',
+              borderBottom: '1px',
+              borderBottomStyle: 'solid',
+              borderBottomColor: 'lightgray',
+            }
+      }
+    />
+  );
+};
 
-export default Square
+export default Square;
